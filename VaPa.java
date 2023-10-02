734a71be2907ff0c734b586fa5a5513bf16eb852
Yash Sharma <98091956+yashsh568@users.noreply.github.com>
2023-10-02 13:34:31 +0530

    Update VaPa.java

--- VaPa.java
+++ VaPa.java
@@ -1,15 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-
-/**
- /*
- * @author adrianadewunmi
- * Participant Login Validation Servlet
- * for registering new participants.
- */
-
 import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -17,22 +5,8 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author adrianadewunmi
- */
 public class VaPa extends HttpServlet {
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
